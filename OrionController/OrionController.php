689192b481ab4b51513e83da957724d062865118
kumarishan <kumar.ishan.opensource@gmail.com>
2011-01-20 16:16:25 +0530

    modified:   OrionController.php 	new file:   ../OrionScheduler/OrionScheduler.php

--- OrionController/OrionController.php
+++ OrionController/OrionController.php
@@ -4,7 +4,18 @@ class OrionController {
 
   public static function getRemoteClassObject($className, $session=false){
   
-    $nodelList = OrionRemoteObjectDirectory::getNodes($className);
+    if(OrionRemoteObjectDirectory::checkEntry($className, $session) == false ){
+     OrionRemoteObjectDirectory::createEntry($className, $session);
+     $info = OrionRemoteObjectDirectory::getEntry($className, $session);
+     $infoMessage = OrionRMS::getInfoMessage($info, 'REMOTEOBJECTCREATION');
+     foreach($info['nodes'] as $node ){
+       self::sendMessage($node, 'OrionController', $infoMessage);
+     }
+    }else{
+      $info = OrionRemoteObjectDirectory::getEntry($className, $session);
+    }
+    $objStub = OrionRFC::getRemoteObjectStub($info);   
+    return $objStub;
 
   }
 
@@ -12,7 +23,17 @@ class OrionController {
   
   }
 
+  public static function sendMessage($node, $service, $message){
+
+  }
   
+  public static function fetchPackage($package){
+
+  }
+
+  public static function createLocalObject($className, $session=false, $params = array()){
+    OrionScheduler::scheduleObject($className, $session, $params);
+  }
 }
 
 ?>
